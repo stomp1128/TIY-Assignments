73af8c686114f1fae31f70a56eeff24df3b1862a
Chris Stomp <christopherstomp@gmail.com>
2015-10-05 16:41:32 -0400

    Adding Coding Kata Week1

---
+++ Coding Kata Week1.playground/Contents.swift
@@ -0,0 +1,15 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+//Question 1
+
+//Multiples of 3 and 5
+
+//If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23. Find the sum of all the multiples of 3 or 5 below 1000.
+
+//Question 2
+
+//Largest Palindrome Product
+
+//A palindromic number reads the same both ways. The largest palindrome made from the product of two 2-digit numbers is 9009 = 91 × 99. Find the largest palindrome made from the product of two 3-digit numbers.
