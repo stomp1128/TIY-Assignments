a5e9ea73444e262f95db47ae19ded4c1e2089707
Chris Stomp <christopherstomp@gmail.com>
2015-10-12 14:43:23 -0400

    Initial commit of SHIELD

---
+++ S.H.I.E.L.D. Hero Tracker/S.H.I.E.L.D. Hero Tracker/HeroDetailViewController.swift
@@ -0,0 +1,14 @@
+//
+//  Hero.swift
+//  S.H.I.E.L.D. Hero Tracker
+//
+//  Created by Chris Stomp on 10/12/15.
+//  Copyright © 2015 The Iron Yard. All rights reserved.
+//
+
+import Foundation
+
+class Hero
+{
+    
+}
