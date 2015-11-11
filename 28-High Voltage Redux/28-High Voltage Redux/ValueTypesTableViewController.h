53ac52f9a5b810e0b8893ee7db753b10b2f338fe
Chris Stomp <christopherstomp@gmail.com>
2015-11-11 13:00:59 -0500

    Initial Commit for 28 - High Voltage Redux

---
+++ 28-High Voltage Redux/28-High Voltage Redux/ValueTypesTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  ElectricityTypesTableViewController.h
+//  28-High Voltage Redux
+//
+//  Created by Chris Stomp on 11/11/15.
+//  Copyright © 2015 The Iron Yard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ValueTypesTableViewController : UITableViewController
+
+@end
---
+++ 28-High Voltage Redux/VoltageCalculatorTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  VoltageCalculatorTableViewController.h
+//  28-High Voltage Redux
+//
+//  Created by Chris Stomp on 11/11/15.
+//  Copyright © 2015 The Iron Yard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VoltageCalculatorTableViewController : UITableViewController
+
+@end
