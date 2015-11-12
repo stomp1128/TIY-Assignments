eb4aba7c3e3744be4b76843e4dd180feef847a0d
Chris Stomp <christopherstomp@gmail.com>
2015-11-12 12:43:13 -0500

    29-MovieMania Initial commit

---
+++ 29-MovieMania/29-MovieMania/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  29-MovieMania
+//
+//  Created by Chris Stomp on 11/12/15.
+//  Copyright © 2015 The Iron Yard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
