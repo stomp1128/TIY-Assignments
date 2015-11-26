cf3eb4678bd0473450dec7bc0b292cc9b625e3ac
Chris Stomp <christopherstomp@gmail.com>
2015-11-26 12:57:57 -0500

    Initial Commit

---
+++ 37-VenueMenu/37-VenueMenu/VenueDetailViewController.swift
@@ -0,0 +1,35 @@
+//
+//  VenueDetailViewController.swift
+//  37-VenueMenu
+//
+//  Created by Chris Stomp on 11/26/15.
+//  Copyright © 2015 The Iron Yard. All rights reserved.
+//
+
+import UIKit
+
+class VenueDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
