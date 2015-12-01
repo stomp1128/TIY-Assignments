e3446dc02e718a6305fccdb32d86b5520f7d049c
Chris Stomp <christopherstomp@gmail.com>
2015-12-01 16:01:36 -0500

    Initial Committ

---
+++ 12-InDueTimeVersion3/12-InDueTimeVersion3/TodoSchedulingViewController.swift
@@ -0,0 +1,41 @@
+//
+//  TodoSchedulingViewController.swift
+//  12-InDueTimeVersion3
+//
+//  Created by Chris Stomp on 12/1/15.
+//  Copyright © 2015 The Iron Yard. All rights reserved.
+//
+
+import UIKit
+
+class TodoSchedulingViewController: UIViewController
+{
+
+    @IBOutlet weak var titleField: UITextField!
+    @IBOutlet weak var deadlinePicker: UIDatePicker!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+    @IBAction func savePressed(sender: UIButton) {
+    }
+
+}
