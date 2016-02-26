a866f903ee42ce79f21b0ca4a1fee682eaa1e54a
Chris Stomp <christopherstomp@gmail.com>
2016-02-25 19:36:01 -0500

    Reorganize code

--- 19 -Forecaster/CityWeatherTableViewController.swift
+++ 19 -Forecaster/CityWeatherTableViewController.swift
@@ -20,47 +20,39 @@ protocol APIControllerProtocol //step 13, add below in class list as well
     func didReceiveDarkSkyAPIResults(results: NSDictionary, city: City)
 }
 
-class CityWeatherTableViewController: UITableViewController, APIControllerProtocol, ChooseCityViewControllerDelegate
-{
-    var api: APIController!
-    var cities = Array<City>()
+class CityWeatherTableViewController: UITableViewController {
+   
+    var cities = [City]()
     var weather: Weather?
     var city: City?
+    var api: APIController!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         
         api = APIController(delegate: self)
-        
         title = "Forecaster"
         
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         self.navigationItem.leftBarButtonItem = self.editButtonItem()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
     }
 
     // MARK: - Table view data source
-
     override func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        return 1
+        return 0
     }
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // #warning Incomplete implementation, return the number of rows
         return cities.count
     }
 
-    
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCellWithIdentifier("CityWeatherCell", forIndexPath: indexPath) as! CityWeatherCell
 
-        
         let city = cities[indexPath.row]
-        
         cell.cityLabel?.text = city.cityName
         
         if city.weather != nil
@@ -81,14 +73,6 @@ class CityWeatherTableViewController: UITableViewController, APIControllerProtoc
         return cell
     }
     
-    func didReceiveZip(zip: String)
-    {
-        self.dismissViewControllerAnimated(true, completion: nil)
-        
-        api = APIController(delegate: self)
-        api.searchForCity(zip)
-    }
-    
     override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?)
     {
         if segue.identifier == "ZipSegue"
@@ -96,44 +80,6 @@ class CityWeatherTableViewController: UITableViewController, APIControllerProtoc
             let zipVC = segue.destinationViewController as! ChooseCityViewController
             zipVC.delegate = self
         }
-    }
-    
-    // MARK: - API Controller Protocol
-    func didReceiveAPIResults(results: NSArray)
-    {
-        dispatch_async(dispatch_get_main_queue(), {
-            //self.cities.append(City.citiesWithJson(results))
-            
-            let city = City.citiesWithJson(results)
-            self.cities.append(city)
-            
-            let api = APIController(delegate: self)
-            api.searchForWeather(city)
-            
-           print(results)
-            self.tableView.reloadData()
-            UIApplication.sharedApplication().networkActivityIndicatorVisible = false
-        })
-    }
-    
-    func didReceiveDarkSkyAPIResults(currently: NSDictionary, city: City)
-    {
-        dispatch_async(dispatch_get_main_queue(),
-            {
-                
-            let weather = Weather.WeatherWithJson(currently)
-            
-            for eachCity in self.cities
-            {
-                if city.cityName == eachCity.cityName
-                {
-                    eachCity.weather = weather
-                }
-            }
-             print(currently)
-            self.tableView.reloadData()
-            UIApplication.sharedApplication().networkActivityIndicatorVisible = false
-        })
     }
     
     override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath)
@@ -148,12 +94,61 @@ class CityWeatherTableViewController: UITableViewController, APIControllerProtoc
     {
         if editingStyle == .Delete {
             
-            //let city = cities[indexPath.row]
             cities.removeAtIndex(indexPath.row)
             
             tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
         } else if editingStyle == .Insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
         }
+    }
+}
+
+extension CityWeatherTableViewController: ChooseCityViewControllerDelegate {
+    
+    func didReceiveZip(zip: String)
+    {
+        self.dismissViewControllerAnimated(true, completion: nil)
+        
+        api = APIController(delegate: self)
+        api.searchForCity(zip)
+    }
+}
+
+extension CityWeatherTableViewController: APIControllerProtocol {
+    // MARK: - API Controller Protocol
+    func didReceiveAPIResults(results: NSArray)
+    {
+        dispatch_async(dispatch_get_main_queue(), {
+            //self.cities.append(City.citiesWithJson(results))
+            
+            let city = City.citiesWithJson(results)
+            self.cities.append(city)
+            
+            let api = APIController(delegate: self)
+            api.searchForWeather(city)
+            
+            print(results)
+            self.tableView.reloadData()
+            UIApplication.sharedApplication().networkActivityIndicatorVisible = false
+        })
+    }
+    
+    func didReceiveDarkSkyAPIResults(currently: NSDictionary, city: City)
+    {
+        dispatch_async(dispatch_get_main_queue(),
+            {
+                let weather = Weather.WeatherWithJson(currently)
+                
+                for eachCity in self.cities
+                {
+                    if city.cityName == eachCity.cityName
+                    {
+                        eachCity.weather = weather
+                    }
+                }
+                print(currently)
+                self.tableView.reloadData()
+                UIApplication.sharedApplication().networkActivityIndicatorVisible = false
+        })
     }
 }
